5eb4de136a6fcbedd788e19a3a539ec935d5dcf7
Tamara Jan <tamara.jan@hotmail.com>
2022-07-01 14:17:30 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+let legendary = 'LEGENDARY';
+let legen = prompt('How I Met Your Mother? It was Legen...Wait for it...');
+let dary = 'dary';
+
+
+while (legen != dary) {
+    alert('Wait for it...');
+    legen = prompt('How I Met Your Mother? It was Legen...Wait for it...');
+}
+
+alert('How I Met Your Mother? It was...' + legendary);
+
