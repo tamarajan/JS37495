9d24bdcec122801c58f3672b525dd62ed652e1a7
Tamara Jan <tamara.jan@hotmail.com>
2022-07-01 17:56:43 -0300

    While con alert + resultado

--- js/script.js
+++ js/script.js
@@ -2,10 +2,17 @@
 let legendary = 'LEGENDARY!';
 let legen = prompt('How I Met Your Mother? It was Legen...Wait for it...');
 let dary = 'dary';
+let wrong = 'Wrong.';
+let wrongAgain = 'Yeah, you are wrong again.'
+let wrongLast = 'Really? Wrong, AGAIN.'
 
 
 while (legen != dary) {
-    alert('Wait for it...');
+    alert('Wait for it...'+ wrong);
+    legen = prompt('How I Met Your Mother? It was Legen...Wait for it...');
+    alert('Wait for it...'+ wrongAgain);
+    legen = prompt('How I Met Your Mother? It was Legen...Wait for it...');
+    alert('Wait for it...'+ wrongLast);
     legen = prompt('How I Met Your Mother? It was Legen...Wait for it...');
 }
 
