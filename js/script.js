86b34eb5d45d7a8e8bd40f08dfbe33d727d44ff9
Tamara Jan <tamara.jan@hotmail.com>
2022-07-04 20:41:55 -0300

    eliminé ramaOperadores y agregué toLowerCase

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ let wrongAgain = 'Yeah, you are wrong again.'
 let wrongLast = 'Really? Wrong, AGAIN.'
 
 
-while (legen != dary) {
+while (legen.toLowerCase() != dary) {
     alert('Wait for it...'+ wrong);
     legen = prompt('How I Met Your Mother? It was Legen...Wait for it...');
     alert('Wait for it...'+ wrongAgain);
