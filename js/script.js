c161083db8e53b39c66fbd39b5223a311d2b6d3e
Tamara Jan <tamara.jan@hotmail.com>
2022-07-11 12:15:47 -0300

    Sumé sindicato

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ let calcularNeto = () => {
     let sueldoNeto = document.getElementById("sueldoNeto")
 
 
-    
+
     let jubilacion = parseInt(montoBruto) * 0.11;
     console.log('La Jubilacion es el pago mensual de una suma de dinero que el Estado paga a las personas que han concluido su ciclo laboral. Los aportes por este motivo son del 11% del salario bruto y se transfieren a la ANSeS.');
     let obraSocial = parseInt(montoBruto) * 0.03;
@@ -12,16 +12,35 @@ let calcularNeto = () => {
     let pami = parseInt(montoBruto) * 0.03;
     console.log('La ley 19032, es la Ley por la cual se crea el Instituto Nacional de Servicios Sociales para Jubilados y Pensionados (creación del PAMI). Es un descuento que se realiza a los trabajadores activos para brindarle asistencia social y salud a los jubilados y pensionados. Los aportes por este motivo son del 3% del salario bruto y se transfieren a PAMI.');
 
-    
 
 
-    montoBruto = montoBruto - jubilacion - obraSocial - pami
+    let sindicato = document.getElementById("sindicato").value
+
+
+    switch (sindicato) {
+        case "no":
+            sindicato = 0;
+            break;
+        case "si":
+            sindicato = parseInt(montoBruto) * 0.02;
+            console.log('Un sindicato es una asociación integrada por trabajadores en defensa y promoción de sus intereses sociales, económicos y profesionales. El aporte al sindicato (generalmente del 1% al 3% del sueldo bruto) es voluntario para quienes decidan afiliarse a su sindicato de actividad.');
+            break;
+        default:
+            alert("Por favor, ingresá si o no.");
+            break;
+    }
+
+
+
+
+
+    montoBruto = montoBruto - jubilacion - obraSocial - pami - sindicato
     console.log('El sueldo neto es el dinero total que percibe un trabajador de bolsillo por el trabajo que desempeña luego de que al mismo se le efectúen las correspondientes retenciones (descuentos).');
 
 
     sueldoNeto.innerHTML = `
         <div>
-        <h3>Tu sueldo neto es de $${montoBruto} Pesos Argentinos</h3>
+        <h3>Tu sueldo neto es de ${montoBruto} pesos argentinos</h3>
         </div>
     `
 }
