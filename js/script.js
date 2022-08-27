637e7c4c1464f036fdc770ac7d5f4b34a8e4f5ab
Tamara Jan <tamara.jan@hotmail.com>
2022-08-27 14:45:12 -0300

    No crea html

--- js/script.js
+++ js/script.js
@@ -83,87 +83,6 @@ function mostrarInformación(event) {
 }
 
 
-async function bringData() {
-    const response = await fetch('./js/data.json');
-    const data = await response.json();
-    console.log(data);
-    crearHTML(data);
-}
-
-window.onload = bringData();
-
-const container = document.querySelector('#adicionales');
-
-
-let crearHTML = (array) => {
-    array.forEach((adicional) => {
-        const checkbox = `
-        <input type="checkbox" id="checkbox" onchange="calcularAdicionales();">
-        <label for="nombre">${adicional.nombre}: $${adicional.monto}</label>
-        </br>
-        `;
-        container.innerHTML += checkbox;
-    })
-}
-
-
-let calcularAdicionales = () => {
-    if('input[type="checkbox" id="checkbox onchange="calcularAdicionales();"]:checked'.value === true) {
-        return (adicional.monto); 
-    }else{
-        return 0;
-    }
-
-    
-}
-
-
-adicionalesTotal.innerHTML = `
-        <div>
-        <h3>Tu sueldo neto tendrá un adicional de $${calcularAdicionales()} pesos argentinos.</h3>
-        </div>
-        `
-
-
-/*
-
-let calcularAdicionalesDic = () => {
-
-    const adicionales = [
-        { nombre: 'Gastos equipamiento del trabajador', monto: 1600 },
-        { nombre: 'Viáticos', monto: 8000 },
-        { nombre: 'Bono por ventas', monto: 2000 },
-        { nombre: 'Bono Fin de Año', monto: 1600 },
-        { nombre: 'Bono reemplazo Caja Navideña', monto: 2300 },
-    ]
-
-
-    const adicionalesDic = adicionales.reduce((acc, num) => acc + num.monto, 0)
-
-
-
-    adicionalesDiciembre.innerHTML = `
-    <div>
-    <h3>En el mes de Diciembre, tu sueldo neto tendrá un adicional de $${adicionalesDic} pesos argentinos conformados por:</h3>
-    </div>
-`
-    function mostrarAdicionales() {
-        let lista = document.getElementById("ulListadoDic");
-        adicionales.forEach(function (data) {
-            let linew = document.createElement("li");
-            let contenido = document.createTextNode(data.nombre + ": $" + data.monto);
-            lista.appendChild(linew);
-            linew.appendChild(contenido);
-
-        })
-    }
-
-    mostrarAdicionales();
-
-}
-
-*/
-
 
 
 
@@ -241,40 +160,23 @@ btnCalcular.addEventListener('click', () => {
 
 })
 
+
 fetch('https://holidayapi.com/v1/holidays?pretty&key=242bf3a6-0141-4c7b-b1fe-d2a2e50d9ae9&country=AR&year=2021')
 .then((response) => response.json())
 .then((data) => console.log(data));
 
-/*
-const contenedor = document.querySelector('#contenedorTarjetas');
-const container = document.querySelector('#cardContainer');
-const btnBuscar = document.querySelector('#buscar');
 
+const listaFeriados = async () => {
+    const response = await fetch("https://holidayapi.com/v1/holidays?pretty&key=242bf3a6-0141-4c7b-b1fe-d2a2e50d9ae9&country=AR&year=2021");
+    const array = await response.json();
 
-
-let crearHTMLAPI = (array) =>  {
-    contenedor.innerHTML = '';
-    container.innerHTML = '';
-    array.forEach((holidays) => {
-        const tarjeta = `
-            <div class="col">
-                <div class="card h-100">
-                    <div class="card-body">
-                        <h5 class="card-title">Nombre: ${holidays.name}</h5>
-                        <p class="card-text">Fecha: ${holidays.date}</p>
-                        <p class="card-text">Día de la Semana: ${holidays.weekday}</p>
-                    </div>
-                </div>
-            </div>`;
-        contenedor.innerHTML += tarjeta;
-    })
-}
-
-btnBuscar.addEventListener('click', () => {
-    fetch('https://holidayapi.com/v1/holidays?pretty&key=242bf3a6-0141-4c7b-b1fe-d2a2e50d9ae9&country=AR&year=2021')
-        .then((response) => response.json())
-        .then((data) => {
-            crearHTMLAPI((data));
-        })
-})
-*/
+    let listaHolidays = ``;
+    Array.from(array).forEach((holidays) => {
+        listaHolidays += `
+        <h3>${holidays.name}</h3>
+        <h3>${holidays.date}</h3>
+        <h3>${holidays.weekday.date.name}</h3>
+        `;
+    });
+    feriados.innerHTML = listaHolidays;
+};
